5a47656f82ce87df2aed7bd4b75187ebd4bc2bd9
xDragan <marcfv97@gmail.com>
2018-09-18 20:27:28 +0200

    we are close

--- ModuleImGui.cpp
+++ ModuleImGui.cpp
@@ -1,70 +1,46 @@
 #include "Globals.h"
 #include "Application.h"
 #include "ModuleImGui.h"
-#include "ImGui/imgui.h"
-#include "ImGui/imgui_impl_opengl2.h"
-#include "ImGui/imgui_impl_sdl.h"
-#include "ImGui/imgui_internal.h"
-#include "ModuleWindow.h"
+#include "ImGui\imgui.h"
+#include "ImGui\imgui_impl_sdl_gl3.h"
 #include "Glew\include\glew.h"
-#include "ImGui/imgui_draw.cpp"
-#include "ImGui/imgui_demo.cpp"
-
+#include "ModuleWindow.h"
 
 #pragma comment( lib, "Glew/libx86/glew32.lib" )
 
-ModuleImGui::ModuleImGui(Application * app, bool start_enabled): Module (app, start_enabled)
+ModuleImGui::ModuleImGui(Application* app, bool start_enabled) : Module(app, start_enabled)
 {
 }
 
 ModuleImGui::~ModuleImGui()
-{
-}
+{}
 
-bool ModuleImGui::Init()
+bool ModuleImGui::Start()
 {
-	LOG("ImGui version used: %s", ImGui::GetVersion());
+	LOG("Starting glew & ImGui");
 	glewInit();
 	ImGui_ImplSdlGL3_Init(App->window->GetWindow());
-
 
 	return true;
 }
 
-
-
 update_status ModuleImGui::PreUpdate(float dt)
 {
 	ImGui_ImplSdlGL3_NewFrame(App->window->GetWindow());
-	return UPDATE_CONTINUE;
+	return(UPDATE_CONTINUE);
 }
 
 update_status ModuleImGui::Update(float dt)
 {
-	//ImGui::Begin("Hola Ricard");
-	if (ImGui::Button("Open Demo", ImVec2(100, 30)))
-	{
-		demo = true;
-	}
-	if (ImGui::Button("Close Demo", ImVec2(100, 30)))
-	{
-		demo = false;
-	}
-	if (ImGui::Button("Close App", ImVec2(100, 30)))
-	{
-		return UPDATE_STOP;
-	}
-	if (demo)
-	{
-		ImGui::ShowTestWindow();
-	}
-	//ImGui::End();
+
+	ImGui::ShowTestWindow();
 	ImGui::Render();
 	return UPDATE_CONTINUE;
 }
 
 bool ModuleImGui::CleanUp()
 {
+	LOG("Unloading Intro scene");
 	ImGui_ImplSdlGL3_Shutdown();
 	return true;
 }
--- ModuleImGui.h
+++ ModuleImGui.h
@@ -1,25 +1,24 @@
-#ifndef __MODULEIMGUI__
-#define __MODULEIMGUI__
+#ifndef __MODULEIMGUI_H__
+#define __MODULEIMGUI_H__
 
 #include "Module.h"
-
-#pragma comment( lib, "Glew/libx86/glew32.lib" )
+#include "Globals.h"
 
 class ModuleImGui : public Module
 {
 public:
-
 	ModuleImGui(Application* app, bool start_enabled = true);
 	~ModuleImGui();
 
-	bool Init();
+	bool Start();
 	bool CleanUp();
 
-	update_status PreUpdate(float dt) override;
-	update_status Update(float dt) override;
+	update_status Update(float dt);
+	update_status PreUpdate(float dt);
 
 	bool demo = false;
+public:
+
 };
 
-
-#endif // __MODULE_IMGUI__			
+#endif // __MODULEIMGUI_H__
